7a4e24a53b1f84b436c13d1cf9b375f4be242d51
MonokaiJs <monokaijs@gmail.com>
2023-06-15 02:12:57 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl, {
+        'customClass': 'style-tooltip'
+    })
+})
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl, 
+        {
+            html: true
+        }
+    )
+})
